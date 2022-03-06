64db244ac582437e814ff002870ef6a3cfcab358
Matthew Pierce <lesbianhaircut@outlook.com>
2022-03-06 14:56:04 -0500

    c

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.scss";
 import React, { useState } from "react";
-import Content from "../components/index";
+import Content from "./components/index";
 export default function App() {
   return (
     <div className="App">
